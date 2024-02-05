3e09dce0aee0da27a72377767ced44a78f08920d
Vladimir <ivanovnetua@gmail.com>
2024-02-05 21:03:00 +0200

    Add filter by id

--- src/app/components/table.component.ts
+++ src/app/components/table.component.ts
@@ -14,30 +14,44 @@ export class TableComponent implements OnInit {
   pagesList: number[] = [];
   visibleProducts: Array<ProductInterface> = [];
   productsOnPage = 0;
+  filteredProducts: Array<ProductInterface> = [];
 
   ngOnInit() {
     this.productsLength = this.products.length;
 
     if (this.productsLength) {
-      this.setPagesList();
+      this.setPagesList(this.productsLength);
     }
 
-    this.setVisibleProducts();
+    this.setVisibleProducts(this.products);
   }
 
-  setPagesList(): void {
-    const pagesQuantity = Math.ceil(this.productsLength / this.maxProductsOnPage);
+  setPagesList(productsLength: number): void {
+    const pagesQuantity = Math.ceil(productsLength / this.maxProductsOnPage);
     this.pagesList = Array.from({ length: pagesQuantity }, (j, i) => i + 1);
   }
 
   changePageNumber(event: any, pageNumber: number): void {
     event.preventDefault();
     this.currentPageNumber = pageNumber;
-    this.setVisibleProducts();
+    this.setVisibleProducts(this.filteredProducts || this.products);
   }
 
-  setVisibleProducts(): void {
-    this.visibleProducts = this.products.slice((this.currentPageNumber - 1) * this.maxProductsOnPage, this.currentPageNumber * this.maxProductsOnPage);
+  setVisibleProducts(products: ProductInterface[]): void {
+    this.visibleProducts = products.slice((this.currentPageNumber - 1) * this.maxProductsOnPage, this.currentPageNumber * this.maxProductsOnPage);
     this.productsOnPage = this.visibleProducts.length;
+  }
+
+  onSearchChange(event: Event, filterByKey: any): void {
+    this.currentPageNumber = 1;
+
+    const searchValue = (event.target as HTMLInputElement).value;
+    this.filteredProducts = this.products.filter((product: ProductInterface) => {
+      const productValue = product[filterByKey as keyof typeof product].toString();
+      return (productValue.indexOf(searchValue) !== -1)
+    });
+
+    this.setPagesList(this.filteredProducts.length);
+    this.setVisibleProducts(this.filteredProducts);
   }
 }
